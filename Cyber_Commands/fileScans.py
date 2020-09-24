c54e1a31d7703a0ef953e695dafc99d7f2b41699
Ryan Carr <dryancarr@gmail.com>
2020-09-24 09:19:28 -0700

    Finished media scan and connected to todolist

--- Cyber_Commands/fileScans.py
+++ Cyber_Commands/fileScans.py
@@ -2,9 +2,24 @@ from . import util
 from . import todolist
 import os
 
+illegalPrograms = ["ipscan", "hashcat", "nmap", "netmap"]
+
 
 def mediaScan(args):
-    pass
+    startDirectory = "/home"
+
+    if args:
+        if not os.path.exists(args[0]):
+            print("ERROR: Invalid file path")
+            return
+        startDirectory = args[0]
+    
+    util.scanForFileOfName("*.mp3")
+    util.scanForFileOfName("*.mp4")
+    util.scanForFileOfName("*.jpg")
+    util.scanForFileOfName("*.wav")
+
+    todolist.modifyItem("scan media", True)
 
 
 def passwordScan(args):
